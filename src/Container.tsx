73d08b26b7f38470b7b626d251dcbaaba0e7313b
helinn7 <helio.goncalves@estudante.ifms.edu.br>
2025-06-10 11:01:49 -0300

    container

--- src/Container.tsx
+++ src/Container.tsx
@@ -1,17 +1,25 @@
+import { useState } from "react";
+
 interface ContainerProps {
   nome: string;
 }
-function Container(props:ContainerProps) {
-  const contador = 10;
-  function mudar() {
-    console.log('Mudar');
+
+function Container(props: ContainerProps) {
+  const [texto, setTexto] = useState("");
+
+  function trataInput(event: React.ChangeEvent<HTMLInputElement>) {
+    setTexto(event.currentTarget.value);
+    console.log(event.currentTarget.value);
   }
+
   return (
-  <>
-   <h1>{props.nome}</h1>
-   Valor Contador: {contador}
-   <button onClick={mudar}>Mudar</button>
-  </>
- )
+    <>
+      <h1>{props.nome}</h1>
+      Texto: {texto}  
+      <br />
+      <input type="text" placeholder="Mostrar Texto" onChange={trataInput} />
+    </>
+  );
 }
+
 export default Container;
