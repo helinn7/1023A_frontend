929f94931f25dcad0629eb4e1be4185877031997
helinn7 <helio.goncalves@estudante.ifms.edu.br>
2025-06-16 10:14:45 -0300

    intervalo

---
+++ src/Pagina.tsx
@@ -0,0 +1,26 @@
+function Pagina() {
+  return (
+   <>
+   <header>
+    <div>Logo</div>
+    <nav>
+        <ul>
+            <li>
+                <a href="">Home</a>
+                </li>
+            <li>
+                <a href="">Home</a>
+                </li>
+            <li>
+                <a href="">Home</a>
+                </li>
+        </ul>
+    </nav>
+   </header>
+   <main></main>
+   <footer></footer>
+
+   </>
+  )
+}
+export default Pagina;
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 //import './index.css'
 
-import Container from './Container.tsx'
+//import Container from './Container.tsx'
+import Pagina from './Pagina'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Container nome='Header'/>
+    {/* <Container nome='Header'/>
     <Container nome='Body'/>
-    <Container nome='Footer'/>
+    <Container nome='Footer'/> */}
+    <Pagina/>
   </StrictMode>,
 )
